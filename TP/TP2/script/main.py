d008b88c123394d6bb883d290e158029692e3115
Antoine Sterna <sternaantoine@gmail.com>
2022-05-14 19:25:37 +0200

    Napalm rebuild

--- TP/TP2/script/main.py
+++ TP/TP2/script/main.py
@@ -1,29 +1,19 @@
 """Main Module"""
 # imports methods from local modules
-from time import sleep
-from netmiko_ssh import get_inventory, deploy_config, save_config
-from run_napalm import deploy_config_ospf, backup_config, deploy_bakcup_config
 from create_config import build
+from run_napalm import backup_config, get_inventory, deploy_config_replace, deploy_config_ospf, deploy_bakcup_config, deploy_config_merged
 
 
 def main():
     """Main method to compute everything =>
     1. Build config from Jinja'as templates -> build()
     2. Get inventory and deploy config from files generated -> deploy_config()
-    3. Save all configs in backup_cfg folder -> save_config()"""
+    3. Save all configs in backup_cfg_napalm folder -> backup_config()"""
 
-    # build()
-    # inventory_data = get_inventory("inventory/hosts.json")  # => Get inventory from file
-    # deploy_config(inventory_data)
-    # sleep(10)
-    # inventory_data_to_save = get_inventory("inventory/hosts.json")  # => Get inventory from file
-    # save_config(inventory_data_to_save)
-    # inventory_napalm = get_inventory("inventory/hosts_napalm.json")  # => Get inventory from file
-    # deploy_config_ospf(inventory_napalm)
-    inventory_data_napalm = get_inventory(
-        "inventory/hosts_napalm.json")  # => Get inventory from file
-    backup_config(inventory_data_napalm)
-    deploy_bakcup_config(inventory_data_napalm)
+    build()
+    inventory_napalm = get_inventory("inventory_napalm.json")
+    deploy_config_replace(inventory_napalm)
+    deploy_config_ospf(inventory_napalm)
 
 
 if __name__ == "__main__":
--- TP/TP2/script/run_napalm.py
+++ TP/TP2/script/run_napalm.py
@@ -1,10 +1,19 @@
 '''Napalm'''
 
 import os
+import json
 from napalm import get_network_driver
 
 
-def deploy_config_ospf(inventory_dict):
+def get_inventory(inventory) -> dict:
+    '''Get inventory from file'''
+    with open(f"inventory/{inventory}", encoding="utf8") as json_file:
+        data = json.load(json_file)
+
+    return data
+
+
+def deploy_config_ospf(inventory_dict) -> None:
     '''Deploy config ospf on routers from configs/OSPF_{hostname}.conf'''
     for device in inventory_dict:
         if "R" in device.get('device_name'):
@@ -41,6 +50,64 @@ def deploy_config_ospf(inventory_dict):
             print("Done")
 
 
+def deploy_config_merged(inventory_dict) -> None:
+    '''Deploy config on all devices from configs/{hostname}.conf (using merge)'''
+    for device in inventory_dict:
+        hostname = device.get('device_name')
+        driver = get_network_driver('ios')
+        device.pop('device_name')
+        device_connect = driver(**device)
+        device.update({"device_name": hostname})
+        device_connect.open()
+        device_connect.load_merge_candidate(filename=f'configs/{hostname}.conf')
+        print(f"Changes on device {hostname} :\n", device_connect.compare_config())
+
+        try:
+            choice = input("\nWould you like to commit these changes? [yN]: ")
+
+            if choice.lower() == 'y':
+                device_connect.commit_config()
+                print(f"\nChanges committed on device {hostname}")
+
+            else:
+                device_connect.discard_config()
+                print(f"\nChanges discarded on device {hostname}")
+
+        except NameError as error:
+            print(f"\nError: {error}")
+
+        device_connect.close()
+
+
+def deploy_config_replace(inventory_dict) -> None:
+    '''Deploy config on all devices from configs/{hostname}.conf (using replace)'''
+    for device in inventory_dict:
+        hostname = device.get('device_name')
+        driver = get_network_driver('ios')
+        device.pop('device_name')
+        device_connect = driver(**device)
+        device.update({"device_name": hostname})
+        device_connect.open()
+        device_connect.load_replace_candidate(filename=f'configs/{hostname}.conf')
+        print(f"Changes on device {hostname} :\n", device_connect.compare_config())
+
+        try:
+            choice = input("\nWould you like to commit these changes? [yN]: ")
+
+            if choice.lower() == 'y':
+                device_connect.commit_config()
+                print(f"\nChanges committed on device {hostname}")
+
+            else:
+                device_connect.discard_config()
+                print(f"\nChanges discarded on device {hostname}")
+
+        except NameError as error:
+            print(f"\nError: {error}")
+
+        device_connect.close()
+
+
 def backup_config(inventory_dict):
     '''Backup config for all devices in inventory'''
     for device in inventory_dict:
@@ -61,7 +128,7 @@ def backup_config(inventory_dict):
             file.write(output)
 
 
-def deploy_bakcup_config(inventory_dict):
+def deploy_bakcup_config(inventory_dict) -> None:
     '''Deploy backup config from backup_cfg_napalm/{hostname}.bak for all devices in inventory'''
     for device in inventory_dict:
         hostname = device.get('device_name')
